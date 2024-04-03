ce3a825ba6e1f7ffa794e40c7e9cd760def5b43f
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-04-03 13:04:44 +0700

    templating engine and home

---
+++ resources/views/layout/home/main.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Modernize Free</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="{{@asset('assets/css/styles.min.css')}}" />
+  <link rel="stylesheet" href="{{@asset('assets/css/home.css')}}">
+  <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    .html{
+        font-family: 'Poppins' !important;
+    }
+    .wrapper {
+    background-image: url("{{@asset('assets/images/backgrounds/bg_home.png')}}");
+    background-size: 100%;
+    background-repeat: no-repeat;
+    height: 100vh;
+    
+}
+.img_hero{
+    width: 221px;
+    height: 35x;
+}
+.text_hero{
+    font-size: 30px;
+}
+  </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"><img src="{{@asset('assets/images/logos/logo.png')}}" alt=""></a>
+            </div>
+          </nav>
+          <div class="container-fluid">
+            <div class="row align-items-center" style="height: 70vh;">
+                <div class="col-md-6 align-items-center">
+                    <div class="">
+                        <img src="{{@asset('assets/images/logos/logo.png')}}" class="img_hero" alt="">
+                        <p class="text_hero mt-4">Selamat datang di Sistem informasi <br> Manajemen warga. Kelola dan akses <br>informasi warga di SIMA.</p>
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="{{@asset('assets/images/content/img_hero.png')}}" alt="">
+                </div>
+            </div>
+          </div>
+    </div>
+  <script src="{{@asset('assets/libs/jquery/dist/jquery.min.js')}}"></script>
+  <script src="{{@asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{@asset('assets/js/sidebarmenu.js')}}"></script>
+  <script src="{{@asset('assets/js/app.min.js')}}"></script>
+  <script src="{{@asset('assets/libs/apexcharts/dist/apexcharts.min.js')}}"></script>
+  <script src="{{@asset('assets/libs/simplebar/dist/simplebar.js')}}"></script>
+  <script src="{{@asset('assets/js/dashboard.js')}}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/',[HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index']);
+    Route::get('/kelola-warga', [AdminController::class, 'index']);
+    Route::get('/kelola-rt', [AdminController::class, 'index']);
+    Route::get('/kelola-rw', [AdminController::class, 'index']);
+    Route::get('/kelola-umkm', [AdminController::class, 'index']);
+    Route::get('/kelola-iuran', [AdminController::class, 'index']);
+    Route::get('/laporan-iuran', [AdminController::class, 'index']);
 });
