55200d59b0f00550a9b45edd3717fba56d8dbe27
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-04-06 09:53:46 +0700

    Add Comment to explain

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,17 +23,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Call factory for KartuKeluarga table
         KartuKeluargaModel::factory(5)->create();
+
+        // Call RoleSeeder to put in db
         $this->call(RoleSeeder::class);
+
+        // Call UserFactory for user table
         UserModel::factory(10)->create();
+
+        // Call factory for PengaduanWarga table
         PengaduanWargaModel::factory(10)->create();
+
+        // Call factory for Surat table
         SuratModel::factory(10)->create();
+
+        // Call factory for DetailSurat table
         DetailSuratModel::factory(10)->create();
+
+        // Call factory for KegiatanWarga table
         KegiatanWargaModel::factory(10)->create();
+
+        // Call factory for Umkm table
         UmkmModel::factory(10)->create();
+
+        // Call factory for LokasiUmkm table
         LokasiUmkmModel::factory(10)->create();
+
+        // Call factory for PengajuanBansos table
         PengajuanBansosModel::factory(5)->create();
+
+        // Call factory for PenerimaanBansos table
         PenerimaanBansosModel::factory(5)->create();
+
+        // Call factory for Iuran table
         IuranModel::factory(5)->create();
     }
 }
