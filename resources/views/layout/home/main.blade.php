3fcdc2fb1e430086e445cc0556aa1d831688ea32
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-04-17 08:54:33 +0700

    dropdown sidebar

--- resources/views/layout/home/main.blade.php
+++ resources/views/layout/home/main.blade.php
@@ -9,10 +9,12 @@
   <link rel="stylesheet" href="{{@asset('assets/css/styles.min.css')}}" />
   <link rel="stylesheet" href="{{@asset('assets/css/home.css')}}">
   <style>
-
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     .html{
         font-family: 'Poppins' !important;
+    }
+    img{
+      pointer-events: none;
     }
     .wrapper {
     background-image: url("{{@asset('assets/images/backgrounds/bg_home.png')}}");
