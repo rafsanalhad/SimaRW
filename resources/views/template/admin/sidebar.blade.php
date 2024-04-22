60e6884aee498707873ded11f2bc9c91d46198fd
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-04-22 09:30:10 +0700

    Edit icon

--- resources/views/template/admin/sidebar.blade.php
+++ resources/views/template/admin/sidebar.blade.php
@@ -29,41 +29,27 @@
           <span class="hide-menu">Menu</span>
         </li>
         <li class="sidebar-item">
-          <a class="sidebar-link" href="#submenu-kelola-warga" data-bs-toggle="collapse" aria-expanded="false">
+          <a class="sidebar-link" href="#submenu-kelola-data" data-bs-toggle="collapse" aria-expanded="false">
             <span>
               <i class="ti ti-article"></i>
             </span>
-            <span class="hide-menu">Kelola Warga</span>
+            <span class="hide-menu">Kelola Data</span>
             <span class="menu-arrow"></span>
           </a>
-          <div class="collapse" id="submenu-kelola-warga">
+          <div class="collapse" id="submenu-kelola-data">
             <ul class="nav flex-column sub-menu" style="margin-left: 40px;">
               <li class="nav-item">
-                <a class="nav-link" href="/admin/kelola-warga">Submenu 1</a>
+                <a class="nav-link" href=" {{ url('/admin/kelola-warga') }}">Kelola Warga</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/admin/tambah-warga">Submenu 2</a>
+                <a class="nav-link" href="/admin/kelola-rt">Kelola RT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/admin/kelola-rw">Kelola RW</a>
               </li>
               <!-- Add more submenu items as needed -->
             </ul>
           </div>
-        </li>
-
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="/admin/kelola-rt" aria-expanded="false">
-            <span>
-              <i class="ti ti-alert-circle"></i>
-            </span>
-            <span class="hide-menu">Kelola RT</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="/admin/kelola-rw" aria-expanded="false">
-            <span>
-              <i class="ti ti-cards"></i>
-            </span>
-            <span class="hide-menu">Kelola RW</span>
-          </a>
         </li>
         <li class="sidebar-item">
           <a class="sidebar-link" href="/admin/kelola-umkm" aria-expanded="false">
@@ -81,59 +67,7 @@
             <span class="hide-menu">Kelola Surat</span>
           </a>
         </li>
-        <li class="nav-small-cap">
-          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-          <span class="hide-menu">AUTH</span>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-            <span>
-              <i class="ti ti-login"></i>
-            </span>
-            <span class="hide-menu">Login</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
-            <span>
-              <i class="ti ti-user-plus"></i>
-            </span>
-            <span class="hide-menu">Register</span>
-          </a>
-        </li>
-        <li class="nav-small-cap">
-          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-          <span class="hide-menu">EXTRA</span>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
-            <span>
-              <i class="ti ti-mood-happy"></i>
-            </span>
-            <span class="hide-menu">Icons</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
-            <span>
-              <i class="ti ti-aperture"></i>
-            </span>
-            <span class="hide-menu">Sample Page</span>
-          </a>
-        </li>
       </ul>
-      <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
-        <div class="d-flex">
-          <div class="unlimited-access-title me-3">
-            <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Upgrade to pro</h6>
-            <a href="https://adminmart.com/product/modernize-bootstrap-5-admin-template/" target="_blank"
-              class="btn btn-primary fs-2 fw-semibold lh-sm">Buy Pro</a>
-          </div>
-          <div class="unlimited-access-img">
-            <img src="../assets/images/backgrounds/rocket.png" alt="" class="img-fluid">
-          </div>
-        </div>
-      </div>
     </nav>
     <!-- End Sidebar navigation -->
   </div>
