3fdde62346ea988c4177a19ae61fc109129fea91
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-04-24 09:42:07 +0700

    edit spasi

--- resources/views/template/admin/sidebar.blade.php
+++ resources/views/template/admin/sidebar.blade.php
@@ -1,82 +1,82 @@
 <aside class="left-sidebar">
-  <!-- Sidebar scroll-->
-  <div>
-    <div class="brand-logo d-flex align-items-center justify-content-between">
-      <a href="./index.html" class="text-nowrap logo-img">
-        <img src="../assets/images/logos/dark-logo.svg" width="180" alt="" />
-      </a>
-      <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-        <i class="ti ti-x fs-8"></i>
-      </div>
+    <!-- Sidebar scroll-->
+    <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+            <a href="./index.html" class="text-nowrap logo-img">
+                <img src="../assets/images/logos/dark-logo.svg" width="180" alt="" />
+            </a>
+            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8"></i>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href=" {{ url('/admin') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-layout-dashboard"></i>
+                        </span>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Menu</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="#submenu-kelola-data" data-bs-toggle="collapse" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-article"></i>
+                        </span>
+                        <span class="hide-menu">Kelola Data</span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="submenu-kelola-data">
+                        <ul class="nav flex-column sub-menu" style="margin-left: 40px;">
+                            <li class="nav-item">
+                                <a class="nav-link" href=" {{ url('/admin/kelola-warga') }}">Kelola Warga</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/admin/kelola-rt') }}">Kelola RT</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/admin/kelola-rw') }}">Kelola RW</a>
+                            </li>
+                            <!-- Add more submenu items as needed -->
+                        </ul>
+                    </div>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ url('/admin/kelola-umkm') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-file-description"></i>
+                        </span>
+                        <span class="hide-menu">Kelola UMKM</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ url('/admin/kelola-surat') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-typography"></i>
+                        </span>
+                        <span class="hide-menu">Kelola Surat</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ url('/admin/kelola-bansos') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-typography"></i>
+                        </span>
+                        <span class="hide-menu">Kelola Bansos</span>
+                    </a>
+                </li>
+        </nav>
+        <!-- End Sidebar navigation -->
     </div>
-    <!-- Sidebar navigation-->
-    <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-      <ul id="sidebarnav">
-        <li class="nav-small-cap">
-          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-          <span class="hide-menu">Home</span>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href=" {{ url('/admin') }}" aria-expanded="false">
-            <span>
-              <i class="ti ti-layout-dashboard"></i>
-            </span>
-            <span class="hide-menu">Dashboard</span>
-          </a>
-        </li>
-        <li class="nav-small-cap">
-          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-          <span class="hide-menu">Menu</span>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="#submenu-kelola-data" data-bs-toggle="collapse" aria-expanded="false">
-            <span>
-              <i class="ti ti-article"></i>
-            </span>
-            <span class="hide-menu">Kelola Data</span>
-            <span class="menu-arrow"></span>
-          </a>
-          <div class="collapse" id="submenu-kelola-data">
-            <ul class="nav flex-column sub-menu" style="margin-left: 40px;">
-              <li class="nav-item">
-                <a class="nav-link" href=" {{ url('/admin/kelola-warga') }}">Kelola Warga</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('/admin/kelola-rt') }}">Kelola RT</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url('/admin/kelola-rw') }}">Kelola RW</a>
-              </li>
-              <!-- Add more submenu items as needed -->
-            </ul>
-          </div>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="{{ url('/admin/kelola-umkm') }}" aria-expanded="false">
-            <span>
-              <i class="ti ti-file-description"></i>
-            </span>
-            <span class="hide-menu">Kelola UMKM</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="{{ url('/admin/kelola-surat') }}" aria-expanded="false">
-            <span>
-              <i class="ti ti-typography"></i>
-            </span>
-            <span class="hide-menu">Kelola Surat</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="{{ url('/admin/kelola-bansos') }}" aria-expanded="false">
-            <span>
-              <i class="ti ti-typography"></i>
-            </span>
-            <span class="hide-menu">Kelola Bansos</span>
-          </a>
-        </li>
-    </nav>
-    <!-- End Sidebar navigation -->
-  </div>
-  <!-- End Sidebar scroll-->
+    <!-- End Sidebar scroll-->
 </aside>
