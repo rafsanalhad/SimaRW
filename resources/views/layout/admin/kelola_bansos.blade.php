6fbfec84ac6d94743164a16e68d7dbb0d653ebef
Arya Chandra Kusuma <aryachandrak14@gmail.com>
2024-04-24 12:00:03 +0700

    bansos

--- resources/views/layout/admin/kelola_bansos.blade.php
+++ resources/views/layout/admin/kelola_bansos.blade.php
@@ -17,6 +17,7 @@
       </ul>
       <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
         <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+          <a href="https://adminmart.com/product/modernize-free-bootstrap-admin-dashboard/" target="_blank" class="btn btn-primary">Download Free</a>
           <li class="nav-item dropdown">
             <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
               aria-expanded="false">
@@ -67,7 +68,7 @@
                         <td>Malang</td>
                         <td>Pengusaha</td>                        
                         <td><a href="" class="btn btn-danger">Ditolak</a></td>
-                        <td><a href="" onclick='showTambahBansos()' class="btn btn-success">Verifikasi</a></td>
+                        <td><a href="#" onclick='showTambahWarga()' class="btn btn-success">Verifikasi</a></td>
                     </tr>
                 </tbody>
             </table>
@@ -75,100 +76,33 @@
     </div>
    
   </div>
-  <div class="modal modal_tambah_bansos" tabindex="-1" role="dialog">
+  <div class="modal modal_bansos" tabindex="-1" role="dialog">
             <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
+                <div class="modal-content d-flex">
                     <div class="modal-header">
                         <h5 class="modal-title">Verifikasi Bansos</h5>
                     </div>
-                    <div class="modal-body">
-                        <form method="POST" class="form-horizontal">
-                            <div class="row mb-2">
-                                <label class="col-2 control-label col-form-label">Nama: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="bansos_nama" name="bansos_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('bansos_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="form-group row mb-2">
-                                <label class="col-2 control-label col-form-label">Alamat: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="level_nama" name="level_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('level_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="form-group row mb-2">
-                                <label class="col-2 control-label col-form-label">Pekerjaan: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="level_nama" name="level_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('level_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>                          
-                            <div class="row mb-2">
-                                <label class="col-2 control-label col-form-label">Agama: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="level_nama" name="level_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('level_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-2">
-                                <label class="col-2 control-label col-form-label">Status: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="level_nama" name="level_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('level_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-2">
-                                <label class="col-2 control-label col-form-label">Pekerjaan: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="level_nama" name="level_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('level_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-2">
-                                <label class="col-2 control-label col-form-label">Alamat: </label>
-                                <div class="col-10">
-                                    <input type="text" class="form-control" id="level_nama" name="level_nama"
-                                        value="{{ old('level_nama') }}" required>
-                                    @error('level_nama')
-                                        <small class="form-text text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary">Simpan</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal"
-                            onclick=hideTambahWarga()>Tutup</button>
+                    <div class="row modal-body">
+                        <div class="col-5" style="background-color: red; margin-left: 10px; height:500px;">
+                        </div>
+                        <div class="container">
+                            <button type="button" class="btn btn-primary">Simpan</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal"
+                                onclick=hideTambahWarga()>Tutup</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         
     <script>
-        function showTambahBansos() {
-            $('.modal_tambah_bansos').modal('show');
-        }  
+        function showTambahWarga() {
+            $('.modal_bansos').modal('show');
+        }
+
+        function hideTambahWarga() {
+            $('.modal_bansos').modal('hide');
+        }      
     </script>
 
 
