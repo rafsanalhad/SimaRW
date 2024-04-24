4d65915d81a78bbeb6b3f1461df256982f94a536
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-04-24 11:09:13 +0700

    dashboard fix

--- resources/views/layout/admin/dashboard.blade.php
+++ resources/views/layout/admin/dashboard.blade.php
@@ -27,7 +27,7 @@
         <div class="card-body">
           <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
             <div class="mb-3 mb-sm-0">
-              <h5 class="card-title fw-semibold">Sales Overview</h5>
+              <h5 class="card-title fw-semibold">Grafik Pengeluaran Iuran</h5>
             </div>
             <div>
               <select class="form-select">
@@ -48,10 +48,13 @@
           <!-- Yearly Breakup -->
           <div class="card overflow-hidden">
             <div class="card-body p-4">
-              <h5 class="card-title mb-9 fw-semibold">Yearly Breakup</h5>
+              <h5 class="card-title mb-9 fw-semibold">Keuangan</h5>
               <div class="row align-items-center">
-                <div class="col-8">
-                  <h4 class="fw-semibold mb-3">$36,358</h4>
+                <div class="col-12">
+                  <div class="d-flex justify-content-center">
+                    <div id="breakup"></div>
+                  </div>
+                  <h4 class="fw-semibold mb-3">Rp. 10.000.000</h4>
                   <div class="d-flex align-items-center mb-3">
                     <span
                       class="me-1 rounded-circle bg-light-success round-20 d-flex align-items-center justify-content-center">
@@ -71,43 +74,9 @@
                     </div>
                   </div>
                 </div>
-                <div class="col-4">
-                  <div class="d-flex justify-content-center">
-                    <div id="breakup"></div>
-                  </div>
-                </div>
+               
               </div>
             </div>
-          </div>
-        </div>
-        <div class="col-lg-12">
-          <!-- Monthly Earnings -->
-          <div class="card">
-            <div class="card-body">
-              <div class="row alig n-items-start">
-                <div class="col-8">
-                  <h5 class="card-title mb-9 fw-semibold"> Monthly Earnings </h5>
-                  <h4 class="fw-semibold mb-3">$6,820</h4>
-                  <div class="d-flex align-items-center pb-1">
-                    <span
-                      class="me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center">
-                      <i class="ti ti-arrow-down-right text-danger"></i>
-                    </span>
-                    <p class="text-dark me-1 fs-3 mb-0">+9%</p>
-                    <p class="fs-3 mb-0">last year</p>
-                  </div>
-                </div>
-                <div class="col-4">
-                  <div class="d-flex justify-content-end">
-                    <div
-                      class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                      <i class="ti ti-currency-dollar fs-6"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div id="earning"></div>
           </div>
         </div>
       </div>
@@ -118,7 +87,7 @@
       <div class="card w-100">
         <div class="card-body p-4">
           <div class="mb-4">
-            <h5 class="card-title fw-semibold">Recent Transactions</h5>
+            <h5 class="card-title fw-semibold">Detail Pengeluaran Baru</h5>
           </div>
           <ul class="timeline-widget mb-0 position-relative mb-n5">
             <li class="timeline-item d-flex position-relative overflow-hidden">
@@ -127,7 +96,7 @@
                 <span class="timeline-badge border-2 border border-primary flex-shrink-0 my-8"></span>
                 <span class="timeline-badge-border d-block flex-shrink-0"></span>
               </div>
-              <div class="timeline-desc fs-3 text-dark mt-n1">Payment received from John Doe of $385.90</div>
+              <div class="timeline-desc fs-3 text-dark mt-n1">beli semen buat bangun pos ronda</div>
             </li>
             <li class="timeline-item d-flex position-relative overflow-hidden">
               <div class="timeline-time text-dark flex-shrink-0 text-end">10:00 am</div>
@@ -135,8 +104,8 @@
                 <span class="timeline-badge border-2 border border-info flex-shrink-0 my-8"></span>
                 <span class="timeline-badge-border d-block flex-shrink-0"></span>
               </div>
-              <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">New sale recorded <a href="javascript:void(0)"
-                  class="text-primary d-block fw-normal">#ML-3467</a>
+              <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">beli semen buat bangun pos ronda <a href="javascript:void(0)"
+                  class="text-primary d-block fw-normal"></a>
               </div>
             </li>
             <li class="timeline-item d-flex position-relative overflow-hidden">
@@ -145,7 +114,7 @@
                 <span class="timeline-badge border-2 border border-success flex-shrink-0 my-8"></span>
                 <span class="timeline-badge-border d-block flex-shrink-0"></span>
               </div>
-              <div class="timeline-desc fs-3 text-dark mt-n1">Payment was made of $64.95 to Michael</div>
+              <div class="timeline-desc fs-3 text-dark mt-n1">beli semen buat bangun pos rond</div>
             </li>
             <li class="timeline-item d-flex position-relative overflow-hidden">
               <div class="timeline-time text-dark flex-shrink-0 text-end">09:30 am</div>
@@ -153,8 +122,8 @@
                 <span class="timeline-badge border-2 border border-warning flex-shrink-0 my-8"></span>
                 <span class="timeline-badge-border d-block flex-shrink-0"></span>
               </div>
-              <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">New sale recorded <a href="javascript:void(0)"
-                  class="text-primary d-block fw-normal">#ML-3467</a>
+              <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">beli semen buat bangun pos rond <a href="javascript:void(0)"
+                  class="text-primary d-block fw-normal"></a>
               </div>
             </li>
             <li class="timeline-item d-flex position-relative overflow-hidden">
@@ -163,7 +132,7 @@
                 <span class="timeline-badge border-2 border border-danger flex-shrink-0 my-8"></span>
                 <span class="timeline-badge-border d-block flex-shrink-0"></span>
               </div>
-              <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">New arrival recorded
+              <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">beli semen buat bangun pos rond
               </div>
             </li>
             <li class="timeline-item d-flex position-relative overflow-hidden">
@@ -171,7 +140,7 @@
               <div class="timeline-badge-wrap d-flex flex-column align-items-center">
                 <span class="timeline-badge border-2 border border-success flex-shrink-0 my-8"></span>
               </div>
-              <div class="timeline-desc fs-3 text-dark mt-n1">Payment Done</div>
+              <div class="timeline-desc fs-3 text-dark mt-n1">beli semen buat bangun pos rond</div>
             </li>
           </ul>
         </div>
@@ -180,25 +149,23 @@
     <div class="col-lg-8 d-flex align-items-stretch">
       <div class="card w-100">
         <div class="card-body p-4">
-          <h5 class="card-title fw-semibold mb-4">Recent Transactions</h5>
+          <h5 class="card-title fw-semibold mb-4">Pengeluaran</h5>
           <div class="table-responsive">
             <table class="table text-nowrap mb-0 align-middle">
               <thead class="text-dark fs-4">
                 <tr>
                   <th class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0">Id</h6>
+                    <h6 class="fw-semibold mb-0">No</h6>
                   </th>
-                  <th class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0">Assigned</h6>
-                  </th>
+               
                   <th class="border-bottom-0">
                     <h6 class="fw-semibold mb-0">Name</h6>
                   </th>
                   <th class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0">Priority</h6>
+                    <h6 class="fw-semibold mb-0">Role</h6>
                   </th>
                   <th class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0">Budget</h6>
+                    <h6 class="fw-semibold mb-0">Jumlah</h6>
                   </th>
                 </tr>
               </thead>
@@ -209,18 +176,13 @@
                   </td>
                   <td class="border-bottom-0">
                     <h6 class="fw-semibold mb-1">Sunil Joshi</h6>
-                    <span class="fw-normal">Web Designer</span>
+                    
                   </td>
                   <td class="border-bottom-0">
-                    <p class="mb-0 fw-normal">Elite Admin</p>
+                    <p class="mb-0 fw-normal">RT</p>
                   </td>
                   <td class="border-bottom-0">
-                    <div class="d-flex align-items-center gap-2">
-                      <span class="badge bg-primary rounded-3 fw-semibold">Low</span>
-                    </div>
-                  </td>
-                  <td class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0 fs-4">$3.9</h6>
+                    <h6 class="fw-semibold mb-0 fs-4">Rp. 10000000</h6>
                   </td>
                 </tr>
                 <tr>
@@ -229,18 +191,12 @@
                   </td>
                   <td class="border-bottom-0">
                     <h6 class="fw-semibold mb-1">Andrew McDownland</h6>
-                    <span class="fw-normal">Project Manager</span>
                   </td>
                   <td class="border-bottom-0">
-                    <p class="mb-0 fw-normal">Real Homes WP Theme</p>
+                    <p class="mb-0 fw-normal">RW</p>
                   </td>
                   <td class="border-bottom-0">
-                    <div class="d-flex align-items-center gap-2">
-                      <span class="badge bg-secondary rounded-3 fw-semibold">Medium</span>
-                    </div>
-                  </td>
-                  <td class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0 fs-4">$24.5k</h6>
+                    <h6 class="fw-semibold mb-0 fs-4">10000000</h6>
                   </td>
                 </tr>
                 <tr>
@@ -249,18 +205,12 @@
                   </td>
                   <td class="border-bottom-0">
                     <h6 class="fw-semibold mb-1">Christopher Jamil</h6>
-                    <span class="fw-normal">Project Manager</span>
                   </td>
                   <td class="border-bottom-0">
-                    <p class="mb-0 fw-normal">MedicalPro WP Theme</p>
+                    <p class="mb-0 fw-normal">RW</p>
                   </td>
                   <td class="border-bottom-0">
-                    <div class="d-flex align-items-center gap-2">
-                      <span class="badge bg-danger rounded-3 fw-semibold">High</span>
-                    </div>
-                  </td>
-                  <td class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0 fs-4">$12.8k</h6>
+                    <h6 class="fw-semibold mb-0 fs-4">10.00000</h6>
                   </td>
                 </tr>
                 <tr>
@@ -269,18 +219,12 @@
                   </td>
                   <td class="border-bottom-0">
                     <h6 class="fw-semibold mb-1">Nirav Joshi</h6>
-                    <span class="fw-normal">Frontend Engineer</span>
                   </td>
                   <td class="border-bottom-0">
-                    <p class="mb-0 fw-normal">Hosting Press HTML</p>
+                    <p class="mb-0 fw-normal">RW</p>
                   </td>
                   <td class="border-bottom-0">
-                    <div class="d-flex align-items-center gap-2">
-                      <span class="badge bg-success rounded-3 fw-semibold">Critical</span>
-                    </div>
-                  </td>
-                  <td class="border-bottom-0">
-                    <h6 class="fw-semibold mb-0 fs-4">$2.4k</h6>
+                    <h6 class="fw-semibold mb-0 fs-4">Rp.100000</h6>
                   </td>
                 </tr>
               </tbody>
