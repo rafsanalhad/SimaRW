ad2c1be6245e3ebba068123518ee289ab3a69aa7
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-05-12 08:53:18 +0700

    fix navbar padding

--- resources/views/layout/home/main.blade.php
+++ resources/views/layout/home/main.blade.php
@@ -17,7 +17,7 @@
 <body>
   <div class="wrapper">
     <nav class="navbar navbar-expand-lg navbar-light shadow-lg fixed-top z-100" style="background: linear-gradient(to left, var(--background-color), #cadeff);">
-      <div class="container-fluid px-5">
+      <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="{{@asset('assets/images/logos/logo.png')}}" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
