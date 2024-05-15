3d8790da647391a0f2f2c8230ccaa5b80b3a44c4
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-05-15 12:16:54 +0700

    Ganti foto di landing page

--- resources/views/layout/home/main.blade.php
+++ resources/views/layout/home/main.blade.php
@@ -91,7 +91,8 @@
                     </div>
                     <div class="col-md-6 d-flex justify-content-center align-items-center">
 
-                        <img class="img_hero" src="{{ @asset('assets/images/content/img_hero.png') }}" alt="">
+                        <img style="width: 100%; height: 100%; border-radius: 36px;" class="img_hero"
+                            src="{{ @asset('assets/images/content/founder1.jpg') }}" alt="">
 
                     </div>
                 </div>
