538b4af93357e7fc214c47e2f67bf251589464b7
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-06-02 13:48:18 +0700

    Add Feature Warga & Admin First Time Login

--- routes/web.php
+++ routes/web.php
@@ -101,7 +101,6 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/ubah-password', [AdminUbahPasswordController::class, 'ubahPassword']);
             Route::post('/ubah-password', [AdminUbahPasswordController::class, 'updatePassword']);
 
-
             // Route get pengumuman
             Route::get('/pengumuman', [PengumumanController::class, 'show']);
             Route::post('/tambah-pengumuman', [PengumumanController::class, 'create']);
