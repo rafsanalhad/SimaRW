18a490dd67f8cbcd36063068f4518828df4ba922
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-06-06 17:12:41 +0700

    Revisi LandingPage

--- resources/views/layout/admin/dashboard.blade.php
+++ resources/views/layout/admin/dashboard.blade.php
@@ -22,6 +22,7 @@
             <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
                 <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
                     <li class="nav-item dropdown">
+                        @include('components.profil')
                         <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="{{ url('/assets/images/profile/user-1.jpg') }}" alt="" width="35"
