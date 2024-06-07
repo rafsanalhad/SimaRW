8e4d012210e45e736c54d1844a2cbb3b307af2f1
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-06-07 09:27:36 +0700

    revisi home

--- resources/views/layout/home/main.blade.php
+++ resources/views/layout/home/main.blade.php
@@ -119,7 +119,7 @@
                             <a href="/login" class="btn btn_hero">Mulai Sekarang</a>
                         </div>
                     </div>
-                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                    <div class="col-md-6 d-flex justify-content-center align-items-center mt-3">
 
                         <img style="width: 100%; height: 100%; border-radius: 36px;" class="img_hero"
                             src="{{ @asset('assets/images/content/fotoRTRW.jpg') }}" alt="">
